6a9d9e2b0b973a2c42b28d715398aa00644973f2
jayakumar9666 <134843730+jayakumar9666@users.noreply.github.com>
2024-01-03 17:12:25 +0530

    updated code

--- setup.py
+++ setup.py
@@ -1,28 +1,15 @@
 from setuptools import setup, find_packages
 from typing import List
 
-HYPEN_E_DOT='-e .'
-
-'''def get_requiremet(file_path:str)->List[str]:
-    requirements = []
-    with open(file_path) as f:
-        requirements=f.readlines()
-        requirements=[req.replace("\n","")for req in requirements]
-        
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-    return requirements'''
-
-   
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 
-__version__ = "0.0.6"
-REPO_NAME = "mongodb_connector"
-PKG_NAME= "MongoConnect"
+__version__ = "0.0.4"
+REPO_NAME = "mongodb-connector"
+PKG_NAME= "databaseautomation"
 AUTHOR_USER_NAME = "medisetti-jayakumar"
-AUTHOR_EMAIL = "medisetti-jayakumar@gmail.com"
+AUTHOR_EMAIL = "medisettijayakumar@gmail.com"
 
 setup(
     name=PKG_NAME,
@@ -38,6 +25,4 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    
-    
-)
+    )
